65098ee203753b5735e6924d658404c6522f0207
Gh0sty23 <maxxyisagh0st@gmail.com>
2025-02-10 12:20:55 +0800

    Components directory, where the modules, codes etc will go, and prototype sidebar ui

---
+++ virtual-backpack/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import './Sidebar.css'
+function Sidebar () {
+  return (
+    <>
+      <div class="sidenav">
+        <a href ="#">Notebook</a>
+        <a href = "#">Flashcards</a>
+        <a href = "#">Calendar</a>
+        <a href = "#">To-Do list</a>
+        <a href = "#">ID</a>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar
