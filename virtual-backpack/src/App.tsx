8049d10b9cb98705c3625e5221d079dbf8ff2005
Gh0sty23 <maxxyisagh0st@gmail.com>
2025-02-27 15:59:42 +0800

    Notebook app

--- virtual-backpack/src/App.tsx
+++ virtual-backpack/src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter} from "react-router-dom"
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar.tsx'
+import NotesApp from "./components/Notebook/NoteApp.tsx"
 
 function App() {
   return (
-    <Sidebar></Sidebar>
+    <>
+      <Sidebar></Sidebar>
+      <BrowserRouter>
+        <NotesApp />
+      </BrowserRouter>
+    </>
   )
 }
 
