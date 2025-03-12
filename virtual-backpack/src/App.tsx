8d679d769bddfbb361e859afbe0c56faa433d204
Charles Dela Cruz <charlesdelacruz360@gmail.com>
2025-03-12 11:20:08 +0800

    ADDED CALENDAR FUNCTION

--- virtual-backpack/src/App.tsx
+++ virtual-backpack/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Sidebar from './components/Sidebar/Sidebar.tsx'
 import NotesApp from "./components/Notebook/NoteApp.tsx"
 import ToDoApp from "./components/todo/ToDoComponent.tsx"
+import Calendar from "./components/Calendar/Calendar.tsx"
+import CalendarApp from "./components/Calendar/CalendarApp.tsx"
 
 function App() {
   return (
     <>
       <Sidebar></Sidebar>
-      <ToDoApp/>
+      <CalendarApp/>
     </>
   )
 }
